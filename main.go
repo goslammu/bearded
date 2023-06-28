1c5f914d672603aa320e9b435a76035ad5098092
gruzdev.ds <gruzdev.ds@inbox.ru>
2023-06-28 23:59:12 +0300

    deleted debugs

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bearded/internal/app/commander"
 	"flag"
-	"fmt"
 	"log"
 )
 
@@ -13,7 +12,6 @@ func main() {
 	flag.BoolVar(&restore, "r", false, "defines if to restore info from dump or not")
 	flag.Parse()
 
-	fmt.Println(restore)
 	cmd, err := commander.New(restore)
 	if err != nil {
 		log.Fatal(err)
